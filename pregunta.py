f2e4210702495c7343aa315b643bcb8f4ad6ef7a
Sebastian Pachon <scpachong@unal.edu.co>
2022-10-17 23:12:50 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -9,24 +9,12 @@ espacio entre palabra y palabra.
 
 
 """
-''''
-first=tbl1[0].replace(' ','')
-second=tbl1[1].replace(' ','_')[-16:-1]
-header1=first[0:7]
-header2=first[7:7+8]+'_'+first[7+8:7+8+2]+second
-header3=first[7+8+2:10+7+8+2]+'_'+first[10+7+8+2:10+7+8+2+2]+second
-header4=first[-25:-14]+second
-headers=[header1,header2,header3,header4]
-'''
-
-from optparse import Values
-from types import WrapperDescriptorType
 import pandas as pd
 import re
 import itertools
 
-def ingest_data():
-    #header
+# break_rows=list(map(lambda x:re.split(r'\s{2,}', x),rows))
+def ingest_data():    
     with open('clusters_report.txt', "r") as file:
         data_header= file.readlines()[0:3]
 
@@ -37,24 +25,20 @@ def ingest_data():
 
     with open('clusters_report.txt', "r") as f:
         raw_text = f.readlines()[4:]
-    text = "\n".join(raw_text)
-    rows=text.split("\n\n\n\n ")
 
-    #clean
-    break_rows=list(map(lambda x:re.split(r'\s{2,}', x),rows))
-    break_rows_clean=list(map(lambda x:x[1:],break_rows))
-    key_words=[break_rows_clean[i][3:] for i in range(len(rows))]
-    kw=" ".join(key_words[0])
-    break_rows_clean[0][2].replace(' %','').replace(',','.')
+    text="\n".join(raw_text)
+    text_point=text.replace("\n\n\n\n",".\n\n\n\n")
+    text_clean=text_point.split(".\n\n")
+    rows=list(map(lambda x:re.split(r'\s{2,}', x),text_clean))
+    break_rows=[rows[i][1:] for i in range(13)]
+    key_words=[break_rows[i][3:] for i in range(len(break_rows))]
+    key_words_clean=list(map(lambda x:' '.join(x).replace('.',''),key_words))
     #Rows
-    cluster=[break_rows_clean[i][0] for i in range(len(rows))]
-    amount_w=[break_rows_clean[i][1] for i in range(len(rows))]
-    percent_w=[break_rows_clean[i][2].replace(' %','').replace(',','.') for i in range(len(rows))]
-
-    key_words_clean=list(map(lambda x:' '.join(x),key_words))
-
-    types=['int','int','float','str']
+    cluster=[int(break_rows[i][0]) for i in range(len(break_rows))]
+    amount_w=[int(break_rows[i][1]) for i in range(len(break_rows))]
+    percent_w=[float(break_rows[i][2].replace(' %','').replace(',','.')) for i in range(len(break_rows))]
     values=[cluster,amount_w,percent_w,key_words_clean]
     d= {k:v for k,v in zip(words_clean,values)}
-    df=pd.DataFrame(d)
+    df=pd.DataFrame(d)  
     return df
+
